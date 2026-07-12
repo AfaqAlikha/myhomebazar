d0b40e771e048dc78ce719a3a6a9121f54d8a569
AfaqAlikha <afaq97326@gmail.com>
2026-07-12 12:43:47 +0500

    push env

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,4 +1,9 @@
+// export const env = {
+//   BASE_URL: 'http://localhost:5000/api',
+//   WEBSOCET_URL: 'http://localhost:5000',
+// };
+
 export const env = {
-  BASE_URL: 'http://localhost:5000/api',
-  WEBSOCET_URL: 'http://localhost:5000',
+  BASE_URL: 'https://myhomebazar.com/api',
+  WEBSOCET_URL: 'https://myhomebazar.com',
 };
